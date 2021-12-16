6a4c3b2560c67b12196ea1e0301a848652ab8f3a
Rodrigo Santos <rodrigo.s.santos003@gmail.com>
2021-12-16 14:12:46 +0000

    Added responsivity to navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50%";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
 function googleLogin() {
   window.open(
     "https://accounts.google.com/signin/v2/identifier?passive=1209600&continue=https%3A%2F%2Faccounts.google.com%2Fb%2F0%2FAddMailService&followup=https%3A%2F%2Faccounts.google.com%2Fb%2F0%2FAddMailService&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
