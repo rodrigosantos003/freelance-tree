bbac0c4f5cdeae4bfd4e5299527c6e674a7f6cb6
Rodrigo Santos <rodrigo.s.santos003@gmail.com>
2022-01-10 16:50:20 +0000

    Changed main js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//hide navbar on scroll
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
@@ -9,12 +10,14 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
+//login with google
 function googleLogin() {
   window.open(
     "https://accounts.google.com/signin/v2/identifier?passive=1209600&continue=https%3A%2F%2Faccounts.google.com%2Fb%2F0%2FAddMailService&followup=https%3A%2F%2Faccounts.google.com%2Fb%2F0%2FAddMailService&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
   );
 }
 
+//login with facebook
 function facebookLogin() {
   window.open("https://www.facebook.com/?ocid=topsitePT-PT");
 }
